2a01f158911b26577b7a247584185388fbafb56c
Mick1406 <mick.carraz@gmail.com>
2023-01-07 08:36:33 +0100

    base skeleton

--- platformer/src/main.rs
+++ platformer/src/main.rs
@@ -1,8 +1,21 @@
-use bevy::prelude::*;
+use bevy::prelude::* ;
+use bevy_asset_loader::prelude::* ;
 
 fn main() {
+    let mut app = App::new() ;
+    LoadingState::new(GameState::AssetLoading)
+        .continue_to_state(GameState::Playing)
+        .with_collection::<ImageAssets>()
+        .build(&mut app) ;
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .run();
+        .run() ;
 }
+
+#[derive(AssetCollection)]
+struct ImageAssets {
+    #[asset(path = "sandbox.ldtk")]
+    map: Handle<LdtkAsset>,
+}
+
